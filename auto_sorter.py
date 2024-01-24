21a96bb6c93100dec93bf50a2dcfaa0dff54445d
EonMurph <stiger.home@gmail.com>
2024-01-24 17:09:03 +0000

    Cleaned up auto_sorter.py

--- auto_sorter.py
+++ auto_sorter.py
@@ -1,16 +1,22 @@
 import frontmatter
+
 import shutil
 from pathlib import Path, PurePath
 from scripts_data import OBSIDIAN_ROOT
 
 # Set values
-tags = {'study': 'module', 'daily': 'date-created',
-        'quarterly': 'year', 'weekly': 'year'}
-tag = ''
-personal_destination = Path(str(OBSIDIAN_ROOT) + '/I-Personal').absolute()
+tags = {
+    "study": "module",
+    "daily": "date-created",
+    "quarterly": "year",
+    "weekly": "year",
+}
+tag = ""
+personal_destination = Path(str(OBSIDIAN_ROOT) + "/I-Personal").absolute()
 
-folders = [path for path in Path(OBSIDIAN_ROOT).glob(
-    '[!".", 0]*/**') if Path.is_dir(path)]
+folders = [
+    path for path in Path(OBSIDIAN_ROOT).glob('[!".", 0]*/**') if Path.is_dir(path)
+]
 
 
 def valid(path):  # function for checking if file is from valid folder
@@ -20,47 +26,59 @@ def valid(path):  # function for checking if file is from valid folder
     return all(part[0] not in ".0" for part in path.parts)
 
 
-files = [path for path in Path(OBSIDIAN_ROOT).rglob(
-    "*.md") if valid(path) and frontmatter.check(path)]
+files = [
+    path
+    for path in Path(OBSIDIAN_ROOT).rglob("*.md")
+    if valid(path) and frontmatter.check(path)
+]
 
 # runs through all valid files and sorts them
 for file in files:
-    if 'type' in frontmatter.load(file).keys():
+    if "type" in frontmatter.load(file).keys():
         note = frontmatter.load(file)
-        if note['type'] in tags:
-            tag = tags[note['type']]
+        if note["type"] in tags:
+            tag = tags[note["type"]]
         # sets default location if doesn't match any if statements
-        destination = Path(OBSIDIAN_ROOT) / 'Unsorted'
+        destination = Path(OBSIDIAN_ROOT) / "Unsorted"
     else:
         continue
 
-    if tag in ['date-created', 'year']:  # checks if file is a daily/quarterly/weekly entry
-        year = month = ''
-        year = note[tag].split('-')[0]  # grabs the year from the frontmatter
-        if tag == 'date-created':
+    if tag in [
+        "date-created",
+        "year",
+    ]:  # checks if file is a daily/quarterly/weekly entry
+        year = month = ""
+        year = note[tag].split("-")[0]  # grabs the year from the frontmatter
+        if tag == "date-created":
             # grabs the month from the frontmatter
-            month = note[tag].split('-')[1]
-            destination = personal_destination / 'Daily' / \
-                year / month  # if month value then daily entry
+            month = note[tag].split("-")[1]
+            destination = (
+                personal_destination / "Daily" / year / month
+            )  # if month value then daily entry
         else:
             # if not then sort based on titled type metadata
-            destination = personal_destination / note['type'].title() / year
+            destination = personal_destination / note["type"].title() / year
 
-    elif tag == 'module':
+    elif tag == "module":
         for folder in folders:
             split_folder = PurePath(folder).name.split(
-                '_')  # split final folder in path
+                "_"
+            )  # split final folder in path
             if split_folder[0] == note[tag]:
                 destination = folder.absolute()  # sets destination based on module code
 
-    elif note[tag] in ['weekly', 'monthly', 'yearly']:  # sorts all other periodic entries
+    elif note[tag] in [
+        "weekly",
+        "monthly",
+        "yearly",
+    ]:  # sorts all other periodic entries
         destination = personal_destination / note[tag].title()
 
-    if not destination in folders:
+    if destination not in folders:
         Path(destination).mkdir(parents=True, exist_ok=True)
 
     if Path(file).parent.absolute() != destination:
         shutil.move(file, (destination))  # moves files if not already sorted
 
-    if destination == Path(OBSIDIAN_ROOT) / 'Unsorted':
-        print(f'!!{file} has not been sorted!!')
+    if destination == Path(OBSIDIAN_ROOT) / "Unsorted":
+        print(f"!!{file} has not been sorted!!")
