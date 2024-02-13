1c51171fdb458f348ee0451552b12153db60d30f
EonMurph <stiger.home@gmail.com>
2024-02-13 08:14:53 +0000

    Created basic script for creating FLASK environment

---
+++ flask_setup.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+cwd = os.getcwd()
+
+if not os.path.exists(f"{cwd}/.flaskenv"):
+    with open(".flaskenv", "w") as outFile:
+        lines = ["FLASK_APP=app.py\n", "FLASK_DEBUG=TRUE"]
+        outFile.writelines(lines)
+
+if not os.path.exists(f"{cwd}/app.py"):
+    with open("app.py", "w") as outFile:
+        lines = ["from flask import Flask, render_template\n\n", "o = Flask(__name__)\n", 'app.config["SECRET_KEY"] = "this-is-my-secret-key"\n\n']
+        outFile.writelines(lines)
+
+if not os.path.exists(f"/templates/"):
+    os.mkdir("templates")
+    
+    with open("templates/base.html", "w") as outFile:
+        pass
+
+
+subprocess.run(["code.cmd", "."])
+subprocess.run(["code.cmd", "app.py"])
